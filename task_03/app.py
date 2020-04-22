d390714cc7ad13c893608022ebce52d12aadc5c6
gleb.mirzazyanov@gmail.com <gleb.mirzazyanov@gmail.com>
2020-04-22 17:33:39 +0300

    fix code_style and delete unused library

--- task_03/app.py
+++ task_03/app.py
@@ -1,10 +1,11 @@
-from flask import Flask, redirect, render_template, request, session, url_for, flash, send_file, current_app, \
+import os
+
+from flask import Flask, redirect, render_template, request, session, url_for, flash, current_app, \
     send_from_directory
 from flask_dropzone import Dropzone
-from flask_uploads import UploadSet, configure_uploads, IMAGES, patch_request_class
-from logic import start_swaping
+from werkzeug.utils import secure_filename
 
-import os
+from logic import start_swapping
 
 app = Flask(__name__)
 dropzone = Dropzone(app)
@@ -12,82 +13,73 @@ dropzone = Dropzone(app)
 app.config['SECRET_KEY'] = 'supersecretkeygoeshere'
 
 # Dropzone settings
-# app.config['DROPZONE_UPLOAD_MULTIPLE'] = False
 app.config['DROPZONE_ALLOWED_FILE_CUSTOM'] = True
 app.config['DROPZONE_ALLOWED_FILE_TYPE'] = 'image/*'
 app.config['DROPZONE_MAX_FILES'] = 1
-app.config['UPLOAD_FOLDER'] = "uploads"
+app.config['UPLOAD_FOLDER'] = os.path.join('static', 'uploads')
 
-# Uploads settings
-app.config['UPLOADED_PHOTOS_DEST'] = os.getcwd() + '/uploads'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
-photos = UploadSet('photos', IMAGES)
-configure_uploads(app, photos)
-patch_request_class(app)  # set maximum file size, default is 16MB
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     # set session for image results
-    if "file_urls" not in session:
-        session['file_urls'] = []
+    if "file_names" not in session:
         session['file_names'] = []
 
-    # list to hold our uploaded image urls
-    file_urls = session['file_urls']
     file_names = session['file_names']
 
     # handle image upload from Dropszone
     if request.method == 'POST':
-        file_obj = request.files
-        for f in file_obj:
-            file = request.files.get(f)
+        file = request.files['file']
 
-            # save the file with to our photos folder
-            filename = photos.save(
-                file,
-                name=file.filename
-            )
-            file_names.append(filename)
-            # append image urls
-            file_urls.append(photos.url(filename))
-        if len(file_urls) % 2 == 0:
+        filename = secure_filename(file.filename)
+        if not allowed_file(filename):
+            return render_template('index.html', error=True,
+                                   message='File should be one of the following formats: jpeg, jpg, png.')
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        file_names.append(filename)
+
+        if len(file_names) % 2 == 0:
             try:
-                start_swaping(file_names[len(file_names) - 2], file_names[len(file_names) - 1])
-                session['file_urls'] = file_urls
-                return redirect(url_for('return_result'))
-            except:
-                session.pop('file_urls', None)
-                session.pop('file_names', None)
-                flash("No face detection. Please upload image with face")
+                first_img = file_names[len(file_names) - 2]
+                second_img = file_names[len(file_names) - 1]
+                result_name = start_swapping(first_img, second_img)
+                file_names.append(result_name)
+                session['file_names'] = file_names
                 return render_template('index.html')
+            except IndexError:
+                session.pop('file_names', None)
+                return render_template('index.html', error=True,
+                                       message='Upload image with face')
 
-        session['file_urls'] = file_urls
         session['file_names'] = file_names
         return "uploading..."
 
     return render_template('index.html')
 
 
-@app.route('/return_result')
+@app.route('/result', methods=['GET', 'POST'])
 def return_result():
-    uploads = os.path.join(current_app.root_path, app.config['UPLOAD_FOLDER'])
-    return send_from_directory(directory=uploads, filename='result.jpg')
-
-
-@app.route('/results', methods=['GET', 'POST'])
-def results():
-    # redirect to home if no images to display
-
-    if "file_urls" not in session or session['file_urls'] == []:
+    if "file_names" not in session or session['file_names'] == []:
         return redirect(url_for('index'))
 
-    file_urls = session['file_urls']
-    # set the file_urls and remove the session variable
-    session.pop('file_urls', None)
+    file_names = session['file_names']
+    first_img = file_names[len(file_names) - 3]
+    second_img = file_names[len(file_names) - 2]
+    result_img = file_names[len(file_names) - 1]
     session.pop('file_names', None)
-
-    return render_template('result.html', file_urls=file_urls)
+    # uploads = os.path.join(current_app.root_path, app.config['UPLOAD_FOLDER'])
+    # return send_from_directory(directory=uploads, filename='result.jpg')
+    return render_template('result.html',
+                           first_file=url_for('static', filename='uploads/' + first_img)
+                           , second_file=url_for('static', filename='uploads/' + second_img)
+                           , result_image=url_for('static', filename='uploads/' + result_img))
 
 
 if __name__ == '__main__':
